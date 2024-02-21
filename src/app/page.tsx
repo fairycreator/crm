514720997bea217b37f9ea17ae19b45904800486
fairycreator <122693095+fairycreator@users.noreply.github.com>
2024-02-21 22:18:39 +0100

    page updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,8 @@ export default function Home() {
       <h1 className="text-xl">Home page</h1>
       <StatusLabel status={Status.Active}>Active</StatusLabel>
       <StatusLabel status={Status.NotActive}>Active</StatusLabel>
-      <StatusLabel status={Status.Active}>Active</StatusLabel>
-      <StatusLabel status={Status.Active}>Active</StatusLabel>
+      <StatusLabel status={Status.Pending}>Active</StatusLabel>
+      <StatusLabel status={Status.Suspended}>Active</StatusLabel>
     </main>
   );
 }
