61c88583a59d7de38422be4338c4512265e4dc6e
fairycreator <122693095+fairycreator@users.noreply.github.com>
2024-04-06 20:20:27 +0200

    reainstaled dependincies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  // Specify the content to be scanned for classes
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+
+  // Define the theme configuration
   theme: {
-    extend: {},
+    extend: {}, // You can extend the default theme here
   },
+
+  // No custom plugins included at the moment
   plugins: [],
 };
