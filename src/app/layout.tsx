97ea9e58bf1b9db3c0c376b39ae13da61ff7b6c7
fairycreator <122693095+fairycreator@users.noreply.github.com>
2024-05-05 22:24:09 +0200

    layout changed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,1 @@
-import React from 'react';
-import Providers from '@/app/components/providers';
-import './globals.css';
 
-interface RootLayoutProps {
-  children: React.ReactNode;
-}
-
-const RootLayout: React.FC<RootLayoutProps> = ({ children }) => {
-  return (
-    <div className="root-layout">
-      <Providers>{children}</Providers>
-    </div>
-  );
-};
-
-export default RootLayout;
